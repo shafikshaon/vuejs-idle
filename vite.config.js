90e8228791c1013fd8062d9c228bcf50a656c4d5
Shafikur Rahman <shafikshaon@gmail.com>
2023-02-18 22:11:02 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,55 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import pkg from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    cssCodeSplit: false,
+    lib: {
+      entry: "src/install.ts",
+      name: pkg.scope,
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: [
+        {
+          format: "esm",
+          globals: {
+            vue: "Vue",
+          },
+          paths: {
+            vue: "https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js",
+          },
+        },
+        {
+          format: "es",
+          globals: {
+            vue: "Vue",
+          },
+        },
+        {
+          format: "cjs",
+          globals: {
+            vue: "Vue",
+          },
+        },
+        {
+          format: "umd",
+          globals: {
+            vue: "Vue",
+          },
+        },
+      ],
+    },
+  },
+});
